0bfacb96a475e99e0f801c1ef847f52f0a0046f9
thrsky <1491474343@qq.com>
2017-04-06 12:44:35 +0800

    更新SuccesskillDao的测试

---
+++ src/test/java/org/seckill/dao/SuccessKillDaoTest.java
@@ -0,0 +1,41 @@
+package org.seckill.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.entity.SecKill;
+import org.seckill.entity.SuccessKill;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by thRShy on 2017/4/6.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class SuccessKillDaoTest {
+
+    @Resource
+    private SuccessKillDao successKillDao;
+    @Test
+
+    //can use
+    public void insertSuccessKill() throws Exception {
+        long userPhone=13964775481L;
+        long id=1001L;
+        int res=successKillDao.insertSuccessKill(id,userPhone);
+        System.out.print("updateCount="+res);
+    }
+
+    @Test
+    //can use
+    public void queryByIdWithSecKill() throws Exception {
+        long id=1000L;
+        SuccessKill successKill=successKillDao.queryByIdWithSecKill(id);
+        System.out.print(successKill);
+        System.out.print(successKill.getSecKill().getName());
+    }
+
+}
